daea5018c69f1c5ade9f1ed842c2c879a1c7b7d4
Hrishikesh Konwar <53933663+Hrishikesh-konwar@users.noreply.github.com>
2025-05-31 15:28:05 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 import express from "express";
 import mongoose from "mongoose";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-
 
 import gymRoutes from "./module/gyms/routes.js";
 import paymentRoutes from "./module/payments/routes.js";
 import memberRoutes from "./module/members/routes.js";
-import cors from 'cors';
+import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(console.log('MongoDb connected'))
-  .catch((err) => console.log('Mongo Error', err));
+  .then(console.log("MongoDb connected"))
+  .catch((err) => console.log("Mongo Error", err));
 
-const port = process.env.PORT
+const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-app.use(cors({ 
-  origin: process.env.FRONTEND_BASE_URL,
-  exposedHeaders: ['x-auth-token'], 
-}));
+const json = process.env.GOOGLE_SERVICE_ACCOUNT_JSON;
+if (!json) throw new Error("Missing GOOGLE_SERVICE_ACCOUNT_JSON");
+const serviceAccountPath = path.join("/tmp", "service-account.json");
+if (!fs.existsSync(serviceAccountPath)) {
+  fs.writeFileSync(serviceAccountPath, json);
+}
+process.env.GOOGLE_APPLICATION_CREDENTIALS = serviceAccountPath;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_BASE_URL,
+    exposedHeaders: ["x-auth-token"],
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -32,6 +43,6 @@ app.use("/user", (req, res) => {
   res.send("Hello World");
 });
 
-app.use("/gyms", gymRoutes)
-app.use("/payments", paymentRoutes)
-app.use("/members", memberRoutes)
+app.use("/gyms", gymRoutes);
+app.use("/payments", paymentRoutes);
+app.use("/members", memberRoutes);
